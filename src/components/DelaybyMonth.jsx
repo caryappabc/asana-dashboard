d28f6235bff0ddf44b603bffc429f03bf7249119
caryappabc <caryappa.cari@gmail.com>
2021-02-09 23:29:15 +0530

    added table to delays charts

--- src/components/DelaybyMonth.jsx
+++ src/components/DelaybyMonth.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
 	BarChart,
 	Bar,
@@ -9,8 +9,36 @@ import {
 	Tooltip,
 	Legend,
 } from "recharts";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+	paper: {
+		padding: "5px",
+	},
+	table: {
+		minWidth: 650,
+		backgroundColor: "grey",
+	},
+	bold: {
+		fontWeight: "bold",
+	},
+	fontcolor: {
+		color: "white",
+	},
+});
 
 const DelaybyMonth = ({ data, type, region, po }) => {
+	const classes = useStyles();
+	const [value, setValue] = useState([]);
+
 	let delayed = data.filter((d) => {
 		return d.Project_status === "Delay";
 	});
@@ -42,29 +70,68 @@ const DelaybyMonth = ({ data, type, region, po }) => {
 
 	for (const month in delaybymonth) {
 		let count = delaybymonth[month].length;
+		let projectname = delaybymonth[month].map((i) => i.Name);
 		plotdata.push({
 			month: month,
 			Num_of_delays: count,
+			pName: projectname,
 		});
 	}
 
+	const activeItem = plotdata[value];
+
+	let handleClick = (event, index) => {
+		if (index !== undefined) {
+			setValue(index);
+		}
+	};
+
 	return (
-		<BarChart
-			width={930}
-			height={350}
-			data={plotdata}
-			maxBarSize={60}
-			margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
-		>
-			<CartesianGrid strokeDasharray="3 3" />
-			<XAxis dataKey="month" />
-			<YAxis />
-			<Tooltip />
-			<Legend />
-			<Bar dataKey="Num_of_delays" fill="#2B4F69">
-				<LabelList dataKey="Num_of_delays" position="top" />
-			</Bar>
-		</BarChart>
+		<React.Fragment>
+			<BarChart
+				width={930}
+				height={350}
+				data={plotdata}
+				maxBarSize={60}
+				margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+			>
+				<CartesianGrid strokeDasharray="3 3" />
+				<XAxis dataKey="month" />
+				<YAxis />
+				<Tooltip content={plotdata.pName} />
+				<Legend />
+				<Bar dataKey="Num_of_delays" fill="#2B4F69" onClick={handleClick}>
+					<LabelList dataKey="Num_of_delays" position="top" />
+				</Bar>
+			</BarChart>
+			<TableContainer className={classes.paper} component={Paper}>
+				<Table className={classes.table} aria-label="simple table">
+					<TableHead>
+						<TableRow>
+							<TableCell className={classes.fontcolor}>
+								<Typography className={classes.bold} varient="h1">
+									Projects
+									{activeItem !== undefined ? ` in ${activeItem.month}` : ""}
+								</Typography>
+							</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{activeItem !== undefined ? (
+							activeItem.pName.map((name) => (
+								<TableRow key={name}>
+									<TableCell className={classes.fontcolor}>{name}</TableCell>
+								</TableRow>
+							))
+						) : (
+							<TableRow>
+								<TableCell>Select to view Project name</TableCell>
+							</TableRow>
+						)}
+					</TableBody>
+				</Table>
+			</TableContainer>
+		</React.Fragment>
 	);
 };
 
--- src/components/DelaybyRegion.jsx
+++ src/components/DelaybyRegion.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
 	BarChart,
 	Bar,
@@ -9,8 +9,35 @@ import {
 	Tooltip,
 	Legend,
 } from "recharts";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+	paper: {
+		padding: "5px",
+	},
+	table: {
+		minWidth: 650,
+		backgroundColor: "grey",
+	},
+	bold: {
+		fontWeight: "bold",
+	},
+	fontcolor: {
+		color: "white",
+	},
+});
 
 const DelaybyRegion = ({ data, type, month, po }) => {
+	const classes = useStyles();
+	const [value, setValue] = useState([]);
 	let delayed = data.filter((d) => {
 		return d.Project_status === "Delay";
 	});
@@ -42,29 +69,68 @@ const DelaybyRegion = ({ data, type, month, po }) => {
 
 	for (const region in delaybyregion) {
 		let count = delaybyregion[region].length;
+		let projectname = delaybyregion[region].map((i) => i.Name);
 		plotdata.push({
 			region: region,
 			Num_of_delays: count,
+			pName: projectname,
 		});
 	}
 
+	const activeItem = plotdata[value];
+
+	let handleClick = (event, index) => {
+		if (index !== undefined) {
+			setValue(index);
+		}
+	};
+
 	return (
-		<BarChart
-			width={930}
-			height={350}
-			data={plotdata}
-			maxBarSize={80}
-			margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
-		>
-			<CartesianGrid strokeDasharray="3 3" />
-			<XAxis dataKey="region" />
-			<YAxis />
-			<Tooltip />
-			<Legend />
-			<Bar dataKey="Num_of_delays" fill="#2B4F69">
-				<LabelList dataKey="Num_of_delays" position="top" />
-			</Bar>
-		</BarChart>
+		<React.Fragment>
+			<BarChart
+				width={930}
+				height={350}
+				data={plotdata}
+				maxBarSize={80}
+				margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+			>
+				<CartesianGrid strokeDasharray="3 3" />
+				<XAxis dataKey="region" />
+				<YAxis />
+				<Tooltip />
+				<Legend />
+				<Bar dataKey="Num_of_delays" fill="#2B4F69" onClick={handleClick}>
+					<LabelList dataKey="Num_of_delays" position="top" />
+				</Bar>
+			</BarChart>
+			<TableContainer className={classes.paper} component={Paper}>
+				<Table className={classes.table} aria-label="simple table">
+					<TableHead>
+						<TableRow>
+							<TableCell className={classes.fontcolor}>
+								<Typography className={classes.bold} varient="h1">
+									Projects
+									{activeItem !== undefined ? ` in ${activeItem.region}` : ""}
+								</Typography>
+							</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{activeItem !== undefined ? (
+							activeItem.pName.map((name) => (
+								<TableRow key={name}>
+									<TableCell className={classes.fontcolor}>{name}</TableCell>
+								</TableRow>
+							))
+						) : (
+							<TableRow>
+								<TableCell>Select to view Project name</TableCell>
+							</TableRow>
+						)}
+					</TableBody>
+				</Table>
+			</TableContainer>
+		</React.Fragment>
 	);
 };
 
